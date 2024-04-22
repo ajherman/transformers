1677b5dab16ff6202c006fdb312fb4097aa26d33
Ari Herman <ajherman@login2.cluster>
2024-04-22 11:26:42 -0700

    create diff submit files for coeus and darwin

---
+++ examples/pytorch/language-modeling/submit_darwin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH --job-name=main
+#SBATCH --time 10:00:00
+#SBATCH -N 6
+#SBATCH -p shared-gpu
+#module load miniconda3
+#source activate /vast/home/ajherman/miniconda3/envs/pytorch
+
+srun -o result.out --ntasks=1 -N 1 python run_clm.py \
+    --model_name_or_path openai-community/gpt2 \
+    --dataset_name wikitext \
+    --dataset_config_name wikitext-2-raw-v1 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --do_train \
+    --do_eval \
+    --output_dir /tmp/test-clm
