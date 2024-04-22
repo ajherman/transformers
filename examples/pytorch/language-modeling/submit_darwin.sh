015b7fcc21232a893fd6545c51f082247b1415cb
Ari Herman <ari.j.herman@gmail.com>
2024-04-22 16:48:38 -0600

    wip

--- examples/pytorch/language-modeling/submit_darwin.sh
+++ examples/pytorch/language-modeling/submit_darwin.sh
@@ -6,7 +6,7 @@
 #module load miniconda3
 #source activate /vast/home/ajherman/miniconda3/envs/pytorch
 
-srun -o result.out --ntasks=1 -N 1 python run_clm.py \
+srun -o result.out --ntasks=1 -N 6 python run_clm.py \
     --model_name_or_path openai-community/gpt2 \
     --dataset_name wikitext \
     --dataset_config_name wikitext-2-raw-v1 \
