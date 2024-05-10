8078f376295bcdf687b2458d5c959792f68ac25f
Ari Herman <ari.j.herman@gmail.com>
2024-05-10 14:09:38 -0600

    wip

--- examples/pytorch/language-modeling/submit_darwin.sh
+++ examples/pytorch/language-modeling/submit_darwin.sh
@@ -3,8 +3,8 @@
 #SBATCH --time 10:00:00
 #SBATCH -N 1           
 #SBATCH --mem=40G  
-#SBATCH -p shared-gpu-ampere
-#SBATCH -C gpu1_memory:40GB
+#SBATCH -p shared-gpu
+##SBATCH -C gpu1_memory:40GB
 module load miniconda3
 source activate /vast/home/ajherman/miniconda3/envs/transformer
 #pip install datasets
@@ -13,10 +13,11 @@ source activate /vast/home/ajherman/miniconda3/envs/transformer
 srun -o result.out --ntasks=1 -N 1 python run_clm.py \
     --model_type gpt2 \
     --tokenizer_name openai-community/gpt2 \
+    --config_overrides="n_embd=1024,n_head=8,n_layer=12,n_positions=102" \
     --dataset_name wikitext \
     --dataset_config_name wikitext-2-raw-v1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
     --do_train \
     --do_eval \
-    --output_dir result_dir
+    --output_dir /tmp/test-clm
