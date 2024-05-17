0fcfc3751798f88588f44f8b5a7498f6f5e7b372
Ari Herman <ari.j.herman@gmail.com>
2024-05-17 11:12:28 -0700

    make bash script for tiny example

---
+++ ari/tiny_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH --job-name=main
+#SBATCH --time 10:00:00
+#SBATCH -N 1           
+#SBATCH -p shared-gpu
+module load miniconda3
+source activate /vast/home/ajherman/miniconda3/envs/transformer
+#pip install datasets
+#export PATH="/vast/home/ajherman/miniconda3/envs/transformer/bin:$PATH"
+
+srun -o result.out --ntasks=1 -N 1 python gpt2_train.py --output_dir /tmp/test-clm --num_train_epochs 10 --config_file tiny.json
