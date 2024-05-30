f58d0d7bae22ecab8fdda539c5a1f26ebf4d3ead
Ari Herman <ari.j.herman@gmail.com>
2024-05-30 09:55:59 -0600

    wip

--- ari/tiny_test.sh
+++ ari/tiny_test.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=main
 #SBATCH --time 10:00:00
 #SBATCH -N 1           
-#SBATCH -p shared-gpu
+#SBATCH -p shared-gpu-ampere
 #SBATCH -C gpu_count:2
 #SBATCH --mem=0
 #SBATCH --exclusive
@@ -20,8 +20,8 @@ source activate /vast/home/ajherman/miniconda3/envs/transformer
 #pip install datasets
 #export PATH="/vast/home/ajherman/miniconda3/envs/transformer/bin:$PATH"
 
-srun -o tiny_test.out --ntasks=1 -N 1 torchrun --nproc_per_node 2 gpt2_train.py --output_dir /tmp/test-clm --num_train_epochs 100 --config_file tiny.json --per_device_train_batch_size 16 --mixed_precision &
+srun -o tiny_test.out --ntasks=1 -N 1 torchrun --nproc_per_node 2 gpt2_train.py --output_dir checkpoints --num_train_epochs 20 --config_file medium.json --per_device_train_batch_size 16 --mixed_precision --save_steps 2000 --load_from_checkpoint &
 
 #nvidia-smi
 
-#srun -o tiny_test.out --ntasks=1 -N 1 torchrun --nproc_per_node 2 gpt2_train.py --output_dir /tmp/test-clm --num_train_epochs 100 --config_file config.json --per_device_train_batch_size 12 --mixed_precision &
+#srun -o tiny_test.out --ntasks=1 -N 1 torchrun --nproc_per_node 2 gpt2_train.py --output_dir /tmp/test-clm --num_train_epochs 100 --config_file config.json --per_device_train_batch_size 12 --mixed_precision --save_steps 2000 &
