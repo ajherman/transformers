a70436d656b30dc6043c59d95b596800412b7eeb
Ari Herman <ari.j.herman@gmail.com>
2024-06-13 10:44:15 -0600

    wip

--- ari/main.sh
+++ ari/main.sh
@@ -20,9 +20,9 @@ source activate /vast/home/ajherman/miniconda3/envs/transformer
 #pip install datasets
 #export PATH="/vast/home/ajherman/miniconda3/envs/transformer/bin:$PATH"
 
-srun -p shared-redstone -w cn0 -o original.out --ntasks=1 -N 1 torchrun --nproc_per_node 4 gpt2_train.py --output_dir original --num_train_epochs 2 --config_file medium.json --per_device_train_batch_size 12 --mixed_precision --save_steps 1000 --logging_steps 50 --eval_steps 50 --gradient_accumulation_steps 1 &
+srun -p shared-redstone -w cn0 -o original.out --ntasks=1 -N 1 torchrun --nproc_per_node 4 gpt2_train.py --output_dir original --num_train_epochs 2 --config_file medium.json --per_device_train_batch_size 12 --mixed_precision --save_steps 1000 --logging_steps 50 --eval_steps 50 --gradient_accumulation_steps 1 --load_from_checkpoint &
 
-srun -p shared-redstone -w cn1 -o relu.out --ntasks=1 -N 1 torchrun --nproc_per_node 4 gpt2_train.py --output_dir relu --num_train_epochs 2 --config_file relu.json --per_device_train_batch_size 12 --mixed_precision --save_steps 1000 --logging_steps 50 --eval_steps 50 --gradient_accumulation_steps 1 &
+srun -p shared-redstone -w cn1 -o relu.out --ntasks=1 -N 1 torchrun --nproc_per_node 4 gpt2_train.py --output_dir relu --num_train_epochs 2 --config_file relu.json --per_device_train_batch_size 12 --mixed_precision --save_steps 1000 --logging_steps 50 --eval_steps 50 --gradient_accumulation_steps 1 --load_from_checkpoint &
 
 
 #nvidia-smi
